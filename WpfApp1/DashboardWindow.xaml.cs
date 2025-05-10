e3511443a283ab24b998f1c4406c03708176dba5
JustAGek <kingofgegypt@gmail.com>
2025-05-10 20:10:54 +0300

    greensidebar

--- WpfApp1/DashboardWindow.xaml.cs
+++ WpfApp1/DashboardWindow.xaml.cs
@@ -10,14 +10,40 @@ namespace WpfApp1
             UserNameText.Text = fullName;
         }
 
+        private void ClearSelection()
+        {
+            DashboardBtn.Tag = null;
+            ManageUsersBtn.Tag = null;
+            PatientsBtn.Tag = null;
+            MedicineBtn.Tag = null;
+        }
+
+        private void DashboardButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearSelection();
+            DashboardBtn.Tag = "Selected";
+            // refresh dashboard if needed
+        }
+
         private void ManageUsersButton_Click(object sender, RoutedEventArgs e)
         {
-            new RegistrationWindow { Owner = this }.ShowDialog();
+            ClearSelection();
+            ManageUsersBtn.Tag = "Selected";
+            // open ManageUsersWindow…
         }
 
         private void PatientsButton_Click(object sender, RoutedEventArgs e)
         {
-            new PatientsWindow { Owner = this }.ShowDialog();
+            ClearSelection();
+            PatientsBtn.Tag = "Selected";
+            // open PatientsWindow…
+        }
+
+        private void MedicineButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearSelection();
+            MedicineBtn.Tag = "Selected";
+            // open MedicineWindow…
         }
     }
 }
